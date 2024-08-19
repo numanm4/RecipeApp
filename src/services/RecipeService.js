7e47bdb07bc5c637553fc2c877c69e139b429d56
numanm4 <numanbnr4@gmail.com>
2024-08-19 15:26:56 -0600

    updates

--- src/services/RecipeService.js
+++ src/services/RecipeService.js
@@ -4,7 +4,7 @@ const apiKey = '53ca3b842f6a45ae969fa4e4068a456b';
 
 export async function searchRecipes(ingredients) {
   try {
-    const response = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${encodeURIComponent(ingredients)}&apiKey=${apiKey}`);
+    const response = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${encodeURIComponent(ingredients)}&apiKey=${apiKey}`); // adding api
     return response.data;
   } catch (error) {
     console.error('Error fetching recipes:', error);
